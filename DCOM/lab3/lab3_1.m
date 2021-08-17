e585c1409420b460faee2aaa0a197dff0d8710e5
Harsh Suthar <bernaullis@gmail.com>
2021-08-17 14:28:12 +0530

    DCOM lab3 added

---
+++ DCOM/lab3/lab3_1.m
@@ -0,0 +1,20 @@
+clc;
+close all;
+clear all;
+
+%error probability vector
+p1 = 0.001:0.001:1;
+
+%Channel capacity vector calculation
+Ci = zeros(1,length(p1));
+for i=1:length(p1)
+    Ci(i)=1+p1(i)*log2(p1(i))+(1-p1(i))*log2(1-p1(i));
+end
+
+% plotting channel capacity vs erro probabilty 
+plot(p1,Ci);
+grid on;
+title('Channel Capacity vs Error Probability');
+xlabel('Error Probability');
+ylabel('Channel Capacity');
+axis([0 1 0 1.1]);
---
+++ DCOM/lab3/lab3_2.m
@@ -0,0 +1,44 @@
+clc;
+close all;
+clear all;
+
+%Probabilty of Inputs
+PX=zeros(1,2);
+PX(1)=input('Enter probabilty for input x1');
+PX(2)=1-PX(1);
+
+%Error probabilty
+p=input('Enter error probability');
+
+%Channel Matrix
+PC=[1-p p;p 1-p];
+
+%Probabilty of Output
+PY=PX*PC;
+
+%Calculating Entropy
+H=0;
+for i=1:length(PY)
+    H=H+PY(i)*log2(PY(i));
+end
+
+H=-H;
+
+%Calculating Mutual Information
+I=H+p*log2(p)+(1-p)*log2(1-p);
+
+%displaying
+disp('oinput matrix P(X):');
+disp(PX);
+
+disp('Channel matrix P(Y/X):');
+disp(PC);
+
+disp('output matrix P(Y):');
+disp(PY);
+
+disp('Entropy of output H(Y): ');
+disp(H);
+
+disp('Mutual Information is ');
+disp(I);
