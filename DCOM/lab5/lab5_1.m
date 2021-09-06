e7f256f6c63747a286d60ef2ef901e7e6f46adcb
Harsh Suthar <bernaullis@gmail.com>
2021-09-06 15:35:27 +0530

    added dcom lab 5

--- DCOM/lab5/lab5_1.m
+++ DCOM/lab5/lab5_1.m
@@ -1,7 +1,6 @@
 % has dependency on generateCode.m make sure it is in same directory as this file is
 
 letters=['a', 'b', 'c', 'd', 'e', 'f'];
-% probabilities = [0.22, 0.26, 0.14, 0.12, 0.1, 0.08, 0.05, 0.03];
 probabilities = [0.3, 0.25, 0.20, 0.12, 0.08, 0.05];
 
 resultCode=cell(1,length(probabilities));
